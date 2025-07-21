3f3482dfdd7abbba8251201294321181fda6434f
BrunoFerreira1990 <111455462+BrunoFerreira1990@users.noreply.github.com>
2025-07-21 16:20:21 -0300

    First commit

---
+++ apesc/src/test/java/com/example/apesc/ApescApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apesc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApescApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
