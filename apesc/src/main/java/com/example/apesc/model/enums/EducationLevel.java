1865bed93ff7be57a0f13cb4aac30e46ef69844a
BrunoFerreira1990 <111455462+BrunoFerreira1990@users.noreply.github.com>
2025-07-22 10:37:51 -0300

    Create EducationLevel and Nationality enum classes

--- apesc/src/main/java/com/example/apesc/model/enums/EducationLevel.java
+++ apesc/src/main/java/com/example/apesc/model/enums/EducationLevel.java
@@ -1,4 +1,19 @@
 package com.example.apesc.model.enums;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@AllArgsConstructor
+@Getter
 public enum EducationLevel {
+
+    ELEMENTARY_SCHOOL("Elementary School"),
+    HIGH_SCHOOL("High School"),
+    HIGHER_SCHOOL("Higher School"),
+    SPECIALIZATION("Specialization School"),
+    MASTER_DEGREE("Master Degree"),
+    DOCTORAL_DEGREE("Doctoral Degree");
+
+    private String displayName;
+
 }
