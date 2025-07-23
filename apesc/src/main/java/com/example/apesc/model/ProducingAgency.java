48c943590b41f1f1cba7f51a447a25b81a337e83
BrunoFerreira1990 <111455462+BrunoFerreira1990@users.noreply.github.com>
2025-07-23 17:01:54 -0300

    Create ProducingAgency model

---
+++ apesc/src/main/java/com/example/apesc/model/ProducingAgency.java
@@ -0,0 +1,26 @@
+package com.example.apesc.model;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Table(name = "producing_agency")
+public class ProducingAgency {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "abbreviation")
+    private String abbreviation;
+
+}
