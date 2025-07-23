de1a42d59fb7bfdac535def166c2768f75f3e7e6
BrunoFerreira1990 <111455462+BrunoFerreira1990@users.noreply.github.com>
2025-07-23 16:10:43 -0300

    Add Long id at Person class

--- apesc/src/main/java/com/example/apesc/model/Person.java
+++ apesc/src/main/java/com/example/apesc/model/Person.java
@@ -17,6 +17,9 @@ public abstract class Person {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "name")
     private String name;
 
     @Column(name = "birth_date")
