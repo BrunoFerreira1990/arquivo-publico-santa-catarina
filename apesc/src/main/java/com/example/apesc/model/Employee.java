34a2ad2508b08cc6433fd0948769c4f0b63b476f
BrunoFerreira1990 <111455462+BrunoFerreira1990@users.noreply.github.com>
2025-07-23 16:12:40 -0300

    Add Anotation column in Employee Class

--- apesc/src/main/java/com/example/apesc/model/Employee.java
+++ apesc/src/main/java/com/example/apesc/model/Employee.java
@@ -1,22 +1,24 @@
 package com.example.apesc.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Table;
 import lombok.*;
 
 @Entity
 @Data
-@Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "employee")
 public class Employee extends Person{
 
+    @Column(name = "registration_number")
     private String registrationNumber;
 
+    @Column(name = "position")
     private String position;
 
+    @Column(name = "section")
     private String section;
 
 }
