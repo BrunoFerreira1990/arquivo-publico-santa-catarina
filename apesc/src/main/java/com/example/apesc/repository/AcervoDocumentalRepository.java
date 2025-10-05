0145968163a867ffdc667219936cd8cb0116cdd7
BrunoFerreira1990 <111455462+BrunoFerreira1990@users.noreply.github.com>
2025-10-05 17:53:16 -0300

    Create repositories for the project

---
+++ apesc/src/main/java/com/example/apesc/repository/AcervoDocumentalRepository.java
@@ -0,0 +1,7 @@
+package com.example.apesc.repository;
+
+import com.example.apesc.model.AcervoDocumental;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface AcervoDocumentalRepository extends JpaRepository<AcervoDocumental, Long> {
+}
---
+++ apesc/src/main/java/com/example/apesc/repository/EntidadeProdutoraRepository.java
@@ -0,0 +1,7 @@
+package com.example.apesc.repository;
+
+import com.example.apesc.model.EntidadeProdutora;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface EntidadeProdutoraRepository extends JpaRepository<EntidadeProdutora, Long> {
+}
