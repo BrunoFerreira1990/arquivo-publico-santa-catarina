aec6eda3e3bda28dea62aec4028df0b0981018c4
BrunoFerreira1990 <111455462+BrunoFerreira1990@users.noreply.github.com>
2025-10-05 17:30:33 -0300

    Criação da entidade ProcedimentoREstauraçao

--- apesc/src/main/java/com/example/apesc/model/Funcionario.java
+++ apesc/src/main/java/com/example/apesc/model/Funcionario.java
@@ -2,8 +2,12 @@ package com.example.apesc.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.*;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Data
@@ -20,5 +24,11 @@ public class Empregado extends Pessoa {
 
     @Column(name = "setor")
     private String setor;
+
+    @OneToMany(mappedBy = "empregado")
+    private List<RegistroConsulta> registroConsultas = new ArrayList<>();
+
+    @OneToMany(mappedBy = "responsavelRestauracao")
+    private List<DiagnosticoRestauracao> diagnosticosRealizados = new ArrayList<>();
 
 }
