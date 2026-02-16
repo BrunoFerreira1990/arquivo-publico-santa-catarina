bd9af73d633df907f397dfc127b6c3e76e906f42
BrunoFerreira1990 <111455462+BrunoFerreira1990@users.noreply.github.com>
2026-02-16 16:20:33 -0300

    Add findByName

--- apesc/src/main/java/com/example/apesc/repository/PesquisadorRepository.java
+++ apesc/src/main/java/com/example/apesc/repository/PesquisadorRepository.java
@@ -1,7 +1,11 @@
 package com.example.apesc.repository;
 
 import com.example.apesc.model.Pesquisador;
+
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PesquisadorRepository extends JpaRepository<Pesquisador, Long> {
+    List<Pesquisador> findByName(String name);
 }
