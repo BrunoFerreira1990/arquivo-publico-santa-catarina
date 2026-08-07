561d8e8c75e730e71c3269ead453b365c686f5d7
BrunoFerreira1990 <111455462+BrunoFerreira1990@users.noreply.github.com>
2026-08-07 18:14:13 -0300

    Add RECEBIDOS and EXPEDIDOS to NaturezaTransacao enum

--- apesc/src/main/java/com/example/apesc/model/enums/NaturezaTransacao.java
+++ apesc/src/main/java/com/example/apesc/model/enums/NaturezaTransacao.java
@@ -2,7 +2,9 @@ package com.example.apesc.model.enums;
 
 public enum NaturezaTransacao {
     RECEBIDOS_DE("recebidos de"),
-    EXPEDIDOS_PARA("expedidos para");
+    EXPEDIDOS_PARA("expedidos para"),
+    RECEBIDOS("recebidos"),
+    EXPEDIDOS("expedidos");
 
     private final String transacao;
 
