0f35de009bb5ffc569721a3b8480308028a08bf7
BrunoFerreira1990 <111455462+BrunoFerreira1990@users.noreply.github.com>
2026-08-10 13:46:48 -0300

    Integrada validação de Funcionario no service layer

--- apesc/src/main/java/com/example/apesc/service/employee/FuncionarioServiceImpl.java
+++ apesc/src/main/java/com/example/apesc/service/employee/FuncionarioServiceImpl.java
@@ -1,9 +1,11 @@
 package com.example.apesc.service.employee;
 
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.example.apesc.model.Funcionario;
 import com.example.apesc.repository.FuncionarioRepository;
+import com.example.apesc.util.FuncionarioValidation;
 
 import lombok.AllArgsConstructor;
 
@@ -12,12 +14,17 @@ import lombok.AllArgsConstructor;
 public class FuncionarioServiceImpl implements FuncionarioService {
 
     private final FuncionarioRepository funcionarioRepository;
+    private final FuncionarioValidation funcionarioValidation;
 
+    @Transactional
     public Funcionario save(Funcionario funcionario) {
+        funcionarioValidation.validateSave(funcionario);
         return funcionarioRepository.save(funcionario);
     }
 
+    @Transactional
     public Funcionario update(Funcionario funcionario) {
+        funcionarioValidation.validateUpdate(funcionario);
         return funcionarioRepository.save(funcionario);
     }
 
