61050daa2aed845cf524bf1b89e9192d69b0d2b6
BrunoFerreira1990 <111455462+BrunoFerreira1990@users.noreply.github.com>
2026-08-19 14:00:01 -0300

    feat(registro-consulta): adiciona metodo search na interface do service

--- apesc/src/main/java/com/example/apesc/service/registroconsulta/RegistroConsultaService.java
+++ apesc/src/main/java/com/example/apesc/service/registroconsulta/RegistroConsultaService.java
@@ -1,14 +1,26 @@
 package com.example.apesc.service.registroconsulta;
 
 import com.example.apesc.model.RegistroConsulta;
+import com.example.apesc.model.enums.TipoConsulta;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface RegistroConsultaService {
-    
+
     RegistroConsulta save(RegistroConsulta registroConsulta);
-    
+
     RegistroConsulta findById(Long id);
-    
+
     void delete(Long id);
-    
+
     RegistroConsulta update(RegistroConsulta registroConsulta);
+
+    List<RegistroConsulta> search(
+            LocalDate dataPesquisa,
+            String nomePesquisador,
+            String nomeFuncionario,
+            TipoConsulta tipoConsulta,
+            Boolean semConsulta
+    );
 }
